ad39d0efb9be4b78db0b6421d73dc8b7f99cba57
zhangkepi <1577676986@qq.com>
2024-05-14 15:12:23 +0800

    实现链表数据结构(mac m1)

---
+++ source/kernel/include/tools/list.h
@@ -0,0 +1,64 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "comm/types.h"
+
+typedef struct _list_node_t {
+    struct _list_node_t * pre;
+    struct _list_node_t * next;
+} list_node_t;
+
+typedef struct _list_t {
+    list_node_t * first;
+    list_node_t * last;
+    int count;
+}list_t;
+
+
+static inline void list_node_init(list_node_t * node) {
+    node->pre = node->next = (list_node_t *)0;
+}
+
+static inline list_node_t * list_node_pre(list_node_t * node) {
+    return node->pre;
+}
+
+static inline list_node_t * list_node_next(list_node_t * node) {
+    return node->next;
+}
+
+static inline int list_is_empty(list_t * list) {
+    return list->count == 0;
+}
+
+static inline int list_count(list_t * list) {
+    return list->count;
+}
+
+static inline list_node_t * list_first(list_t * list) {
+    return list->first;
+}
+
+static inline list_node_t * list_last(list_t * list) {
+    return list->last;
+}
+
+
+void list_init(list_t * list);
+void list_insert_first(list_t * list, list_node_t * node);
+void list_insert_last(list_t * list, list_node_t * node);
+list_node_t * list_remove_first(list_t * list);
+list_node_t * list_remove(list_t * list, list_node_t * remove_node);
+
+
+#define offset_in_parent(parent_type, filed_name)   \
+    ((uint32_t)(&(((parent_type *)0)->field_name)))
+
+#define parent_addr(field_addr, parent_type, filed_name) \
+    (uint32_t)((uint32_t)field_addr - offset_in_parent(parent_type, filed_name))
+
+#define field_2_parent(field_addr, parent_type, filed_name) \
+    (parent_type *)(field_addr ? parent_addr(field_addr, parent_type, filed_name) : 0)
+
+
+#endif
---
+++ source/kernel/tools/list.c
@@ -0,0 +1,83 @@
+#include "tools/list.h"
+
+void list_init(list_t * list) {
+    list->first = list->last = (list_node_t *)0;
+    list->count = 0;
+}
+
+void list_insert_first(list_t * list, list_node_t * node) {
+    node->next = list->first;
+    node->pre = (list_node_t *)0;
+
+    if (list_is_empty(list)) {
+        list->last = list->first = node;
+    } else {
+        list->first->pre = node;
+        list->first = node;
+    }
+    list->count++;
+}
+
+
+void list_insert_last(list_t * list, list_node_t * node) {
+    node->pre = list->last;
+    node->next = (list_node_t *)0;
+
+    if (list_is_empty(list)) {
+        list->first = list->last = node;
+    } else {
+        list->last->next = node;
+        list->last = node;
+    }
+
+    list->count++;
+}
+
+
+list_node_t * list_remove_first(list_t * list) {
+
+    if (list_is_empty(list)) {
+        return (list_node_t *)0;
+    }
+
+    list_node_t * remove_node = list->first;
+    list->first = remove_node->next;
+    if (list->first == (list_node_t *)0) {
+        list->last = (list_node_t *)0;
+    } else {
+        remove_node->next->pre = (list_node_t *)0;
+    }
+
+    list->count--;
+
+    remove_node->next = remove_node->pre = (list_node_t *)0;
+
+    return remove_node;
+}
+
+
+list_node_t * list_remove(list_t * list, list_node_t * remove_node) {
+
+    if (remove_node == list->first) {
+        list->first = remove_node->next;
+    }
+
+    if (remove_node == list->last) {
+        list->last = remove_node->pre;
+    }
+
+    if (remove_node->pre) {
+        remove_node->pre->next = remove_node->next;
+    }
+
+    if (remove_node->next) {
+        remove_node->next->pre = remove_node->pre;
+    }
+
+    remove_node->next = remove_node->pre = (list_node_t *)0;
+
+    return remove_node;
+}
+
+
+
